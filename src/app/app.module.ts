fb904e8aa4c0a6206cb11f019104dfa1da62eb58
Charan Reddy P M <charanreddy.pm@corp.terralogic.com>
2022-05-09 12:32:31 +0530

    modified fetch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagementComponent } from './management/management.component';
+import { HeaderModule} from './header/header.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/top-margin/top-margin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-margin',
+  templateUrl: './top-margin.component.html',
+  styleUrls: ['./top-margin.component.scss']
+})
+export class TopMarginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
